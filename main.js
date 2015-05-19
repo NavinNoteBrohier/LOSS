ee7c117969c7d9233a4213f7e3cd04283882a947
Nav <navin.brohier@gmail.com>
2015-05-19 20:22:39 +1000

    Dishwasher 2
    +fixed errors
    + Addition of Collisions

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 // LOSS Nav & Gillie
 //CONSTANTS
-var canvas = document.getElementById("gameCanvas");
+var canvas = document.getElementById("GAMECANVAS");
 var context = canvas.getContext("2d");
+var Background = document.createElement("img");
+	Background.src  = "background.png"
 
+function Collision(x1, y1, h1, w1, x2, y2, h2, w2)
+{
+	if(y2 + h2 > y1 ||
+	x2 + w2 > x1 ||
+	y1 + h1 < y2 ||
+	x2 + w2 < x2)
+	
+	
+}	
+	
 function run()
 {
 	
-}
-
-
-
-
+	
+	
+	
+	context.drawImage(Background, 0, 0)
+};
 
 (function() {
  var onEachFrame;
