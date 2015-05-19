9ba2e8cf60db16f5066541c072a9f67661c416fc
Nav <navin.brohier@gmail.com>
2015-05-19 21:24:20 +1000

    Construction
    + Wall Textures Y
    + Wall attributes in Object.js

--- objects.js
+++ objects.js
@@ -3,34 +3,37 @@
 //WALLS
 var WallX = {
 
-}
+};
 
 var WallY = {
-	
-}
+	height:640,
+	width:20,
+	image:document.createElement("img")
+};
+Wally.src = "wallY.png";
 
-var WallY1 = {
+var WallX1 = {
 	
-}
+};
 
-var WallY2 = {
+var WallX2 = {
 	
-}
+};
 
 var Door = {
 	
-}
+};
 
 //KEYS
 var Flower = {
 	height : ,
 	width : ,
 	image : document.createElement("img")
-}
+};
 
 //KEY HOLES
 var Vase = {
 	height :,
 	width :,
 	image : document.createElement("img")	
-}
+};
